76e75ff6b461b98274d97408ae5fcaccc9f0f6c2
kamilak <kamila.krupa@o2.pl>
2015-10-26 09:12:17 +0100

    dodaj

--- src/main/java/pl/jwest/wildsnake/application/HomeController.java
+++ src/main/java/pl/jwest/wildsnake/application/HomeController.java
@@ -1,14 +1,20 @@
 package pl.jwest.wildsnake.application;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Kamila on 2015-10-12.
+ */
 
 @Controller
 public class HomeController {
-
+    @Value("${welcome.message}")
+    private String pageMessage;
     @RequestMapping("/")
-    public String mainPage() {
-        return "index";
-    }
-
+    public String mainPage(Model model) {
+        model.addAttribute("pageMessage", pageMessage);
+        return "index";}
 }
